548c1781f1de20fbbaf8d6bc50521a5dc14deb13
Cole Rau <colevrau@gmail.com>
2019-12-31 13:30:08 -0800

    Working version still not completely MVP

--- bin/run.rb
+++ bin/run.rb
@@ -2,14 +2,30 @@ require_relative '../config/environment'
 
 cli = CommandLineInterface.new
 
-cli.greet
-cli.main_menu
-input = cli.get_user_input
-if input == "1" 
-    cli.get_guest_show
-elsif input == "2"
-    cli.add_guest
+response = "1"
+while response != "2"
+    cli.greet
+    cli.main_menu
+    input = cli.get_user_input
+    if input == "1" 
+        cli.get_guest_show
+    elsif input == "2"
+        cli.add_guest
+    elsif input == "3"
+        cli.update_guest_age
+    elsif input == "4"
+        cli.remove_guest
+    end 
+    puts 
+    puts "What would you like to do now?"
+    puts "(Please type in the number next to your desired choice)"
+    puts 
+    puts "1. Return to Main Menu"
+    puts "2. Close Program"
+    response = gets.strip 
 end 
+
+
 
 
 
--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -11,6 +11,8 @@ class CommandLineInterface
         puts "(Type in the number of the option you want to select)"
         puts "1 - See a guest's upcoming show."
         puts "2 - Add a guest to a show."
+        puts "3 - Update a guest's age."
+        puts "4 - Remove a guest from their show."
     end 
 
     def get_user_input
@@ -68,33 +70,105 @@ class CommandLineInterface
         puts "Artist: #{displayed_artist_name}"
         puts "Venue: #{displayed_venue_name} - #{displayed_venue_city}, #{displayed_venue_state}"
         puts "Date: #{displayed_venue_date}"
+
+        
     end 
 
     def add_guest
         puts "What is the guest's name? "
-        name = gets.strip
+        new_guest_name = gets.strip
         puts "How old is the guest? "
-        age = gets.strip 
+        new_guest_age = gets.strip 
         puts "Who is the guest seeing? "
         list_artists
-        artist = gets.strip
+        new_guest_artist = gets.strip
         puts "What show is the guest attending? "
         list_shows
-        show = gets.strip 
+        new_guest_show = gets.strip 
+        binding.pry
+        Guest.create(
+            name: new_guest_name, 
+            age: new_guest_age, 
+            artist_id: new_guest_artist.to_i, 
+            show_id: new_guest_show.to_i)
+        
+        new_guest_artist_name = Artist.all.find { |artist| artist.id == new_guest_artist.to_i }
+        new_guest_show_name = Show.all.find { |show| show.id == new_guest_show.to_i}
+        
+        puts 
+        binding.pry
+        puts "#{new_guest_name} is now seeing #{new_guest_artist_name.name} at #{new_guest_show_name.venue_name}!"
     end 
 
+    # LIST ALL ARTISTS 
     def list_artists
         artist_array = Artist.all.map { |artist| artist.name }
+        id_array = Artist.all.map { |artist| artist.id }
         puts 
-        puts "Options: "
+        puts "Please type in the number next to the desired artist name: "
         puts 
-        artist_array.each do |artist| 
-            puts artist 
-        end 
+
+        i = 0 
+        while i < id_array.length do
+            artist_array.each do |artist| 
+                puts "#{id_array[i]}. #{artist}" 
+                i += 1
+            end
+        end
     end 
 
-    def list_shows 
-        Show.all
+    # LIST ALL SHOWS
+    def list_shows    
+        show_array = Show.all.map { |show| show.venue_name }
+        id_array = Show.all.map { |show| show.id }
+        puts 
+        puts "Please type in the number next to the desired show: "
+        puts 
+        
+        i = 0 
+        while i < id_array.length do
+            show_array.each do |show_name| 
+                puts "#{id_array[i]}. #{show_name}" 
+                i += 1
+            end
+        end
+    end 
+
+    def update_guest_age
+        puts 
+        puts "Please choose which guest to update: "
+        list_guests 
+        guest_id_input = gets.strip 
+        correct_guest = Guest.find_by(id: guest_id_input.to_i)
+        puts "What is the guest's new age?"
+        new_age = gets.strip 
+        correct_guest.update(age: new_age)
+        puts "The guest's new age is #{correct_guest.age}."
+    end 
+    
+    # LIST ALL GUESTS 
+    def list_guests
+        guest_array = Guest.all.map { |guest| guest.name }
+        id_array = Guest.all.map { |guest| guest.id }
+        puts "(Type in the number next to the desired guest)"
+        puts 
+        
+        i = 0 
+        while i < id_array.length do
+            guest_array.each do |guest_name| 
+                puts "#{id_array[i]}. #{guest_name}" 
+                i += 1
+            end
+        end        
+    end 
+
+    def remove_guest 
+        puts 
+        puts "Which guest would you like to remove?"
+        list_guests
+        guest_id_input = gets.strip 
+        correct_guest = Guest.find_by(id: guest_id_input.to_i)
+        correct_guest.destroy
+        puts "#{correct_guest.name} is now removed from their show."
     end 
 end 
-
